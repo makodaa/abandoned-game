4db81d45fa4077808be976f8f19a2c9a5c2e51d8
XLokbu <siggayovincent@gmail.com>
2024-04-27 22:25:35 +0800

    Added Two ending scenes and can now count the Days Survived and Items Collected and Crafted

--- core/src/com/teammerge/abandoned/screens/GameOverScreen.java
+++ core/src/com/teammerge/abandoned/screens/GameOverScreen.java
@@ -9,8 +9,6 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Timer;
 import com.teammerge.abandoned.AbandonedGame;
-import com.teammerge.abandoned.entities.Player;
-import com.teammerge.abandoned.records.Index;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,42 +17,60 @@ public class GameOverScreen implements Screen {
 
     private final AbandonedGame game;
     private final BitmapFont font;
-    private final List<String> displayLeftTexts, displayRightTexts;
-    private int currentTextIndex = 0;
-    private int currentLetterIndex = 0;
+    private final List<String> displayLeftTexts, displayRightTexts, displayCentralTexts;
     private int currentRightLetterIndex = 0;
     private int currentLeftLetterIndex = 0;
+    private int currentCentralLetterIndex = 0;
     private boolean textComplete = false;
     private boolean displayingLeftText;
+    private boolean displayingCentralText;
 
-    Player player;
+    private String gameEndText = "";
+    private final int gameEndingScene, daysPassed, itemsCollected, itemsCrafted;
+    GameScreen gameScreen;
 
-    public GameOverScreen(AbandonedGame game, int mapWidth, int mapHeight) {
+    public GameOverScreen(AbandonedGame game, GameScreen gameScreen) {
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/RobotoCondensed-Light.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = 24;
         font = generator.generateFont(parameter);
         generator.dispose();
-
+        this.gameEndingScene = gameScreen.gameEndingScene;
+        this.daysPassed = gameScreen.daysPassed;
+        this.itemsCollected = gameScreen.itemsCollected;
+        this.itemsCrafted = gameScreen.itemsCrafted;
         this.game = game;
-        player = new Player(new Index(mapWidth / 2, mapHeight / 2));
+
+
 
         displayLeftTexts = new ArrayList<>();
         displayRightTexts = new ArrayList<>();
+        displayCentralTexts = new ArrayList<>();
+
+        System.out.println(gameScreen.gameEndingScene);
 
         // TODO: Modify the text
+
+        if(gameScreen.gameEndingScene == 1){
+            gameEndText = "You forgot to breath and are now completely dead";
+        } else {
+            gameEndText = "A rescue helicopter sees you and are now successfully rescued";
+        }
+        displayCentralTexts.add("Congratulations!\n"+ gameEndText);
+
         displayLeftTexts.add(
                 "Days Survived " +
-                        "\n\nDistance Travelled " +
+                        "\n\nDistance Travelled" +
                         "\n\nItems Gathered" +
                         "\n\nItems Crafted" +
                         "\n\nInjuries Faced" +
                         "\n\nInjuries Treated");
 
-        displayRightTexts.add("0" +
+
+        displayRightTexts.add( daysPassed +
                 "\n\n" + "0" +
-                "\n\n" + "0" +
-                "\n\n" + "0" +
+                "\n\n" + itemsCollected +
+                "\n\n" + itemsCrafted +
                 "\n\n" + "0" +
                 "\n\n" + "0");
         // Add more texts as needed
@@ -62,22 +78,29 @@ public class GameOverScreen implements Screen {
         // Start displaying the first text
         displayCurrentText();
 
-        displayingLeftText = true;
+        displayingCentralText = true;
 
         // Set up input listener
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
             public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-                // If text is fully displayed, go to the next text or game screen
-                if (textComplete) {
-                    nextTextOrGame();
+
+                // If both left and right texts are fully displayed, go to the next text or game screen
+                if (textComplete && currentRightLetterIndex == displayRightTexts.getFirst().length()) {
+                    game.setScreen(new MainMenuScreen(game));
+                } else if (currentCentralLetterIndex == displayCentralTexts.getFirst().length()) {
+                    nextText();
+                } else if (displayingCentralText) {
+                    currentCentralLetterIndex = displayCentralTexts.getFirst().length();
                 } else {
-                    /// complete the text
-                    if(displayingLeftText){
-                        currentLetterIndex = displayLeftTexts.get(currentTextIndex).length();
+                    // Complete the text
+                    if (displayingLeftText) {
+                        currentLeftLetterIndex = displayLeftTexts.getFirst().length();
                     } else {
-                        currentLetterIndex = displayRightTexts.get(currentTextIndex).length();
+                        currentRightLetterIndex = displayRightTexts.getFirst().length();
                     }
+                    // Set textComplete to true to trigger the next step
+                    textComplete = true;
                 }
                 return true;
             }
@@ -93,9 +116,17 @@ public class GameOverScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.begin();
+        font.draw(game.batch,
+                displayCentralTexts.getFirst().substring(0, currentCentralLetterIndex),
+                Gdx.graphics.getWidth() / 2f,
+                Gdx.graphics.getHeight() / 2f,
+                0,
+                Align.center,
+                false);
+
         // Draw the left text substring up to the current letter index
         font.draw(game.batch,
-                displayLeftTexts.get(currentTextIndex).substring(0, currentLeftLetterIndex),
+                displayLeftTexts.getFirst().substring(0, currentLeftLetterIndex),
                 100,
                 Gdx.graphics.getHeight() - 150,
                 0,
@@ -104,7 +135,7 @@ public class GameOverScreen implements Screen {
 
         // Draw the right text substring up to the current letter index
         font.draw(game.batch,
-                displayRightTexts.get(currentTextIndex).substring(0, currentRightLetterIndex),
+                displayRightTexts.getFirst().substring(0, currentRightLetterIndex),
                 Gdx.graphics.getWidth() - 100,
                 Gdx.graphics.getHeight() - 150,
                 0,
@@ -139,22 +170,22 @@ public class GameOverScreen implements Screen {
         Timer.schedule(new Timer.Task() {
             @Override
             public void run() {
-                if (displayingLeftText) {
-                    if (currentLeftLetterIndex < displayLeftTexts.get(currentTextIndex).length()) {
+                if(displayingCentralText){
+                    if(currentCentralLetterIndex < displayCentralTexts.getFirst().length()){
+                        currentCentralLetterIndex++;
+                    }
+                } else if (displayingLeftText) {
+                    if (currentLeftLetterIndex < displayLeftTexts.getFirst().length()) {
                         currentLeftLetterIndex++;
                     } else {
                         // If all letters are displayed, mark textComplete as true
-                        textComplete = true;
                         displayingLeftText = false; // Switch to displaying right text
-                        currentRightLetterIndex = 0; // Reset letter index for the next text
                         displayCurrentText(); // Start displaying the right text
                     }
                 } else {
-                    if (currentRightLetterIndex < displayRightTexts.get(currentTextIndex).length()) {
+                    if (currentRightLetterIndex < displayRightTexts.getFirst().length()) {
                         currentRightLetterIndex++;
                     } else {
-                        // If all letters are displayed, mark textComplete as true
-                        textComplete = true;
                         this.cancel();
                     }
                 }
@@ -162,16 +193,12 @@ public class GameOverScreen implements Screen {
         }, 0, 0.2f);
     }
 
-    private void nextTextOrGame() {
-        currentTextIndex++;
-        if (currentTextIndex < displayLeftTexts.size()) {
-            // If there are more texts to display, reset currentLetterIndex and start displaying the next text.
-            currentLetterIndex = 0;
-            textComplete = false;
+    private void nextText(){
+        if (displayingCentralText){
+            currentCentralLetterIndex = 0;
+            displayingLeftText = true;
+            displayingCentralText = false;
             displayCurrentText();
-        } else {
-            // If all texts are displayed, transition to the game screen
-            game.setScreen(new MainMenuScreen(game));
         }
     }
 
